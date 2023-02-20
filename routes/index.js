772f29824cd20c3aa199de118a9b9b8ca6765787
Alexis Flores <94066066+AlexisFlo@users.noreply.github.com>
2023-02-20 14:57:03 +0000

    🐱‍👤Random icon

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let iconSet = ["⭐","🖖","🐱‍👤"];
+  let icon = iconSet[Math.floor(Math.random() * iconSet.length)];
+  res.render('index', { title: 'DWPCII-2023A', icon });
 });
 
 module.exports = router;
